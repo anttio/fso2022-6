022d3316edf96cf23b256870c1f10b5f4bae0ef2
Antti Onttonen <antti@neubit.fi>
2022-09-13 14:12:19 +0300

    part_6 exercise_6.5

---
+++ exercise_6.5/src/App.js
@@ -0,0 +1,56 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+const App = () => {
+  const anecdotes = useSelector((state) =>
+    [...state].sort((a, b) => b.votes - a.votes)
+  );
+  const dispatch = useDispatch();
+
+  const createAnecdote = (content) => {
+    return {
+      type: 'ADD',
+      data: { content },
+    };
+  };
+
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    dispatch(createAnecdote(content));
+  };
+
+  const voteAnecdote = (id) => {
+    return {
+      type: 'VOTE',
+      data: { id },
+    };
+  };
+
+  const vote = (id) => {
+    dispatch(voteAnecdote(id));
+  };
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
