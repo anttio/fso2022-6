2af4c3a541e99962238d831463eeba8ec757f385
Antti Onttonen <antti@neubit.fi>
2022-09-15 18:30:07 +0300

    part_6 exercise_6.15

---
+++ exercise_6.15/src/reducers/anecdoteReducer.js
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+import anecdoteService from '../services/anecdotes';
+
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState: [],
+  reducers: {
+    createAnecdote(state, action) {
+      state.push(action.payload);
+    },
+    voteAnecdote(state, action) {
+      const id = action.payload;
+      const anecdoteToChange = state.find((anecdote) => anecdote.id === id);
+
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1,
+      };
+      return state.map((anecdote) =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      );
+    },
+    setAnecdotes(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export const { createAnecdote, voteAnecdote, setAnecdotes } =
+  anecdoteSlice.actions;
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
+
+export default anecdoteSlice.reducer;
