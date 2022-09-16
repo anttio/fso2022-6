35e85f78eb82f44e905e2fa44343ee04a2ddae5f
Antti Onttonen <antti@neubit.fi>
2022-09-16 14:56:00 +0300

    part_6 exercise_6.17

---
+++ exercise_6.17/src/components/AnecdoteList.js
@@ -0,0 +1,40 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { voteAnecdote } from '../reducers/anecdoteReducer';
+import { setNotification } from '../reducers/notificationReducer';
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch();
+
+  const anecdotes = useSelector(({ anecdotes, filter }) =>
+    [...anecdotes]
+      .sort((a, b) => b.votes - a.votes)
+      .filter((anecdote) => {
+        if (anecdote.content) {
+          return anecdote.content.toLowerCase().includes(filter.toLowerCase());
+        }
+        return null;
+      })
+  );
+
+  const vote = (id) => {
+    const votedAnecdote = anecdotes.find((anecdote) => anecdote.id === id);
+    dispatch(voteAnecdote(votedAnecdote));
+    dispatch(setNotification(`you voted '${votedAnecdote.content}'`));
+  };
+
+  return (
+    <div>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
